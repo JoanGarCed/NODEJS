0a0193f26e0733fd74d1b30569b1712db1c6bc23
JoanGarCed <joan.garced@gmail.com>
2021-01-28 13:38:07 -0700

    promesas.js

---
+++ 02 - Fundamentos JS/promesas.js
@@ -0,0 +1,52 @@
+const empleados = [
+    {
+        id: 1,
+        nombre: 'Fernando'
+    },
+    {
+        id: 2,
+        nombre: 'Linda'
+    },
+    {
+        id: 3,
+        nombre: 'Karen'
+    }
+];
+
+const salarios = [
+    {
+        id: 1,
+        salario: 1000
+    },
+    {
+        id: 2,
+        salario: 1500
+    }
+];
+
+
+const getEmpleado = (id) => {
+    
+
+    const promesa = new Promise( (res, rej) => {
+
+        const empleado = empleados.find(e => e.id === id)
+
+        if(empleado) {
+            res(empleado.nombre);
+        }else{
+            rej(`No existe empleado con id ${id}`);
+        }
+
+    });
+
+    return promesa
+
+    
+}
+
+const id = 4;
+
+getEmpleado(id)
+    .then(empleado => console.log(empleado))
+    .catch(err => console.log(err));
