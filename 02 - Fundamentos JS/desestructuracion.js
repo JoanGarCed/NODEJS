07acdd0165ddd72e40da1037f45b7697a5bd8de5
JoanGarCed <joan.garced@gmail.com>
2021-01-27 22:14:28 -0700

    desestructuracion

--- 02 - Fundamentos JS/desestructuracion.js
+++ 02 - Fundamentos JS/desestructuracion.js
@@ -2,13 +2,21 @@ const deadpool = {
     nombre: 'Wade',
     apellido: 'Winston',
     poder: 'Regeneración',
+    edad:  50,
     getNombre() {
         return `${this.nombre} ${this.nombre} ${this.poder}`;
     }
 }
 
-const nombre = deadpool.nombre;
-const apellido = deadpool.apellido;
-const poder = deadpool.poder;
+// const nombre = deadpool.nombre;
+// const apellido = deadpool.apellido;
+// const poder = deadpool.poder;
 
-console.log(nombre, apellido, poder);
+// const { nombre, apellido, poder, edad = 0 } = deadpool;
+// console.log(nombre, apellido, poder, edad);
+
+function imprimeHeroe({nombre, apellido, poder, edad = 0}){
+    console.log(nombre, apellido, poder, edad);
+}
+
+imprimeHeroe(deadpool);
