e6080436f35d509f1052c72d4f52385df0750732
JoanGarCed <joan.garced@gmail.com>
2021-01-28 21:28:50 -0700

    tabla

--- 03 - Bases-Node/app.js
+++ 03 - Bases-Node/app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 //Imprimir la tabla de multiplicar 
 // 5 x 1 = 5
 //..
@@ -8,11 +10,17 @@ console.log('======================');
 console.log('Tabla del: 5');
 console.log('======================');
 
-const cinco = 5;
+const base = 3;
 let salida = '';
 
 for(let i = 1; i <= 10; i++){
-    salida += `${cinco} x ${i} = ${(cinco * i)} `;
+    salida += `${base} x ${i} = ${(base * i)}\n`;
 }
 
 console.log(salida);
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if(err) throw err;
+
+    console.log('table-5.txt creado');
+});
