47c9a13c14c7433ab0c9cb81ccc81395a6f582e9
JoanGarCed <joan.garced@gmail.com>
2021-01-30 11:36:05 -0700

    yargs exports

--- 03 - Bases-Node/config/yargs.js
+++ 03 - Bases-Node/config/yargs.js
@@ -4,13 +4,15 @@ const argv = require('yargs')
     .option('b', {
         alias: 'base',
         type: 'number',
-        demandOption: true
+        demandOption: true,
+        describe: 'Es la base de la tabla de multiplicar'
     })
     .option('l', {
         alias: 'listar',
         type: 'boolean',
         demandOption: true,
-        default: false
+        default: false,
+        describe: 'Muestra la tabla en consola'
     })
     .check((argv, options) => {
             if(isNaN(argv.b)){
