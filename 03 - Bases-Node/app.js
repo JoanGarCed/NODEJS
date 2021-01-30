5a82ac16538b49c42ef17b20ec9514e516c32266
JoanGarCed <joan.garced@gmail.com>
2021-01-29 23:51:00 -0700

    yargs

--- 03 - Bases-Node/app.js
+++ 03 - Bases-Node/app.js
@@ -1,4 +1,5 @@
 const {crearArchivo} = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
 
 //Imprimir la tabla de multiplicar 
 // 5 x 1 = 5
@@ -7,12 +8,18 @@ const {crearArchivo} = require('./helpers/multiplicar');
 
 console.clear();
 
-const [ , ,arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
-console.log(base);
+console.log(process.argv);
+console.log(argv);
+
+
+console.log('base: yargs', argv.base);
+
+// const [ , ,arg3 = 'base=5'] = process.argv;
+// const [, base = 5] = arg3.split('=');
+// console.log(base);
 
 //const base = 20;
 
-crearArchivo(base)
-    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-    .catch(err => console.log(err));
+// crearArchivo(base)
+//     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+//     .catch(err => console.log(err));
