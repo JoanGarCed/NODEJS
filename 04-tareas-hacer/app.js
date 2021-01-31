aa71bc9a5f236815424ca85e4785762cd4ac784a
JoanGarCed <joan.garced@gmail.com>
2021-01-31 12:27:10 -0700

    lista de opciones

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,11 +1,14 @@
 require('colors');
-const { mostrarMenu } = require('./helpers/mensajes');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 
 const main = async() => {
     console.clear();
     console.log('Hola Mundo');
+    
     mostrarMenu();
+
+    //pausa();
 }
 
 main();
