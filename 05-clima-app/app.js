a83f1dab67f7c2efff9cea8c6da977b61dbcb70e
JoanGarCed <joan.garced@gmail.com>
2021-02-03 00:13:12 -0700

    fin de app

--- 05-clima-app/app.js
+++ 05-clima-app/app.js
@@ -22,22 +22,36 @@ const main = async() => {
                 const lugares = await busquedas.ciudad(termino);
                 // Seleccionar el lugar
                 const id = await listarLugares(lugares);
-                const lugarSel = lugares.find( l => l.id === id);
-                console.log(lugarSel);
-                // Clima
 
+                if(id === '0') continue;
+
+                // Guardar en DB
+                const lugarSel = lugares.find( l => l.id === id);
+                busquedas.agregarHistorial( lugarSel.nombre );
+                // Clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
                 // Mostrar resultados
+                console.clear();
                 console.log('\nInformación de la ciudad\n'.green);
-                console.log('Ciudad:', lugarSel.nombre );
+                console.log('Ciudad:', lugarSel.nombre.green );
                 console.log('Lat', lugarSel. lat);
                 console.log('Lng:', lugarSel. lng);
-                console.log('Temperatura:', );
-                console.log('Maxima: ',);
-                console.log('Minima: ', );
+                console.log('Temperatura:', clima.temp);
+                console.log('Maxima: ', clima.max);
+                console.log('Minima: ', clima.min);
+                console.log('Como está el clima: ', clima.desc.green);
+            break;
+
+            case "2":
+                busquedas.historialCapitalizado.forEach( (lugar, i) => {
+                    
+                    const idx = `${i + 1}.`.green;   
+                    console.log(`${idx} ${lugar}`);
+                });
             break;
         }
 
-        await pausa();
+        if(opt!== 0) await pausa();
     
     }while(opt !== '0');
 
