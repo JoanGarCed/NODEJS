b4338d3170023e125a4106a7a2d6bf214196916e
JoanGarCed <joan.garced@gmail.com>
2021-02-03 09:26:13 -0700

    servir contenido estatico

--- 06-web-server/server.js
+++ 06-web-server/server.js
@@ -1,24 +1,19 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.use( express.static( __dirname + '/public') );
+
+// app.get('/', function (req, res) {
     
-    let salida = {
-        nombre: 'Joan',
-        edad: 29,
-        url: req.url
-    }
+//     let salida = {
+//         nombre: 'Joan',
+//         edad: 29,
+//         url: req.url
+//     }
 
-    res.send(salida);
+//     res.send(salida);
 
-});
-
-app.get('/data', function (req, res) {
-    
-    res.send('Hola Data');
-
-});
-
+// });
 
 app.listen(3000, () => {
     console.log('Escuchando el peticiones en el puerto 3000')
