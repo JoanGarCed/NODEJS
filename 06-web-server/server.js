49fa662df676c33d885ba16e2124171e28f24e06
JoanGarCed <joan.garced@gmail.com>
2021-02-03 11:55:01 -0700

    handlebars

--- 06-web-server/server.js
+++ 06-web-server/server.js
@@ -3,17 +3,17 @@ var app = express();
 
 app.use( express.static( __dirname + '/public') );
 
-// app.get('/', function (req, res) {
+// Express HBS engine
+app.set('view engine', 'hbs');
+
+app.get('/', function (req, res) {
     
-//     let salida = {
-//         nombre: 'Joan',
-//         edad: 29,
-//         url: req.url
-//     }
+    res.render('home',{
+        nombre: 'Joan',
+        anio: new Date().getFullYear()
+    });
 
-//     res.send(salida);
-
-// });
+});
 
 app.listen(3000, () => {
     console.log('Escuchando el peticiones en el puerto 3000')
