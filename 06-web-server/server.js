c0cad8875bdb9872161e40a188bea81a6bf7ed1a
JoanGarCed <joan.garced@gmail.com>
2021-02-05 15:55:02 -0700

    handlebars

--- 06-web-server/server.js
+++ 06-web-server/server.js
@@ -5,12 +5,16 @@ const puerto = 3000;
 //Servir contenido estatico
 app.use(express.static('public'));
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola mundo en su respectiva ruta');
+app.get('/generic', (req, res) => {
+    res.sendFile( __dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+    res.sendFile( __dirname + '/public/elements.html');
 });
     
 app.get('*', (req, res) => {
-    res.send('404  | Page not found');
+    res.sendFile( __dirname + '/public/404.html');
 });
     
 
