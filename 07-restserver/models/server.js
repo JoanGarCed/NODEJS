124d8bbf778ea4bb77ed134b908d571f38c6b29d
JoanGarCed <joan.garced@gmail.com>
2021-02-15 10:39:33 -0700

    router

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -25,29 +25,7 @@ class Server{
 
     routes() {
 
-        this.app.get('/api', (req, res) => {
-            res.status(403).json({
-                msg: 'get API'
-            });
-        });
-
-        this.app.put('/api', (req, res) => {
-            res.status(403).json({
-                msg: 'put API'
-            });
-        });
-
-        this.app.post('/api', (req, res) => {
-            res.status(403).json({
-                msg: 'post API'
-            });
-        });
-
-        this.app.delete('/api', (req, res) => {
-            res.status(403).json({
-                msg: 'delete API'
-            });
-        });
+        this.app.use('/api/usuarios', require('../routes/user'));
 
     }
 
