37490cd4f733fc1e22ace16b220a0d7f3610cc58
JoanGarCed <joan.garced@gmail.com>
2021-02-23 11:58:10 -0700

    proyecto facetería conexión a base de datos

---
+++ 08-restserver-cafeteria/database/config.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {
+
+    try{
+
+    }catch(error){
+        throw new Error('Error a la hora de iniciar la base de datos');
+    }
+
+}
+
+
+module.exports = {
+    dbConnection
+}
